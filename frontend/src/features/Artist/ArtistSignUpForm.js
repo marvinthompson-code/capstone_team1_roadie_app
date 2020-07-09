be9f820ce8b6d4640b33ca085ad7dcd65c2df1fb
Henrynunez112 <henrynunez@pursuit.org>
2020-07-09 15:25:40 -0400

    added one more div to the artist signUp fomr

--- frontend/src/features/Artist/ArtistSignUpForm.js
+++ frontend/src/features/Artist/ArtistSignUpForm.js
@@ -1,85 +1,129 @@
-import React, { useState }from 'react';
-import axios from 'axios';
-import { updateArtist } from '../Artist/artistSlice'
-import { useHistory } from 'react-router-dom';
-import { useDispatch } from 'react-redux';
-import { apiURL } from '../../util/apiURL';
-import { signUp } from '../../util/firebaseFunctions'
+import React, { useState } from "react";
+import axios from "axios";
+import { updateArtist } from "../Artist/artistSlice";
+import { useHistory } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { apiURL } from "../../util/apiURL";
+import { signUp } from "../../util/firebaseFunctions";
 
 const ArtistSignUpForm = () => {
-    const [ name, setName ] = useState("");
-    const [ email, setEmail ] = useState("")
-    const [ city, setCity ] = useState("")
-    const [ password, setPassword ] = useState("")
-    const [ profilePicUrl, setProfilePicUrl ] = useState("")
-    const [ genre, setGenre ] = useState("");
-    const [ bio, setBio ] = useState("");
-    const [ pricing, setPricing ] = useState("");
-    const [ contact, setContact ] = useState("");
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [city, setCity] = useState("");
+  const [password, setPassword] = useState("");
+  const [profilePicUrl, setProfilePicUrl] = useState("");
+  const [genre, setGenre] = useState("");
+  const [bio, setBio] = useState("");
+  const [pricing, setPricing] = useState("");
+  const [contact, setContact] = useState("");
 
-    const API = apiURL();
-    const history = useHistory();
-    const dispatch = useDispatch();
+  const API = apiURL();
+  const history = useHistory();
+  const dispatch = useDispatch();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        try {
-            let res = await signUp(email, password);
-            debugger
-            console.log("Show Artist", res);
-            let res2 = await axios.post(`${API}/artists/`, {
-                id: res.user.uid,
-                name,
-                profilePicUrl,
-                bio,
-                pricing,
-                genre,
-                city,
-                contact
-            })
-            debugger
-            dispatch(updateArtist(res.user));
-            // history.push("/") to feed or artist profile, depends on what we want to do
-        } catch (error) {
-            console.log(error.message);
-        }
-        debugger
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      let res = await signUp(email, password);
+      debugger;
+      console.log("Show Artist", res);
+      let res2 = await axios.post(`${API}/artists/`, {
+        id: res.user.uid,
+        name,
+        profilePicUrl,
+        bio,
+        pricing,
+        genre,
+        city,
+        contact,
+      });
+      debugger;
+      dispatch(updateArtist(res.user));
+      // history.push("/") to feed or artist profile, depends on what we want to do
+    } catch (error) {
+      console.log(error.message);
     }
+    debugger;
+  };
 
-    return (
-        <div className={"FormContainer"}>
-            <div className="artistSignUpTitle">
-            <h3>Artist Sign Up</h3>
-            </div>
-            <form onSubmit={handleSubmit}>
-                <div className="artistInput">
-                <input type={"text"} placeholder={"email"} value={email} onChange={(e) => setEmail(e.currentTarget.value)} required></input>
-                <input type={"password"} placeholder={"password"} value={password} onChange={(e) => setPassword(e.currentTarget.value)} required></input>
-                <input type={"text"} placeholder={"Band Name"} value={name} onChange={(e) => setName(e.currentTarget.value)} required></input>
-                <input type={"text"} placeholder={"genre"} value={genre} onChange={(e) => setGenre(e.currentTarget.value)} required></input>
-                <input type={"text"} placeholder={"contact"} value={contact} onChange={(e) => setContact(e.currentTarget.value)} required></input>
-                <input type={"text"} placeholder={"bio"} value={bio} onChange={(e) => setBio(e.currentTarget.value)}></input>
-                <input type={"text"} placeholder={"city"} value={city} onChange={(e) => setCity(e.currentTarget.value)}></input>
-                </div>
-                {/* Set Range: */}
-                {/* <select>
+  return (
+    <div className={"FormContainer"}>
+      <div className="artistSignUpTitle">
+        <h3>Artist Sign Up</h3>
+      </div>
+      <div className="artistSignUpForm">
+        <form onSubmit={handleSubmit}>
+          <div className="artistInput">
+            <input
+              type={"text"}
+              placeholder={"email"}
+              value={email}
+              onChange={(e) => setEmail(e.currentTarget.value)}
+              required
+            ></input>
+            <input
+              type={"password"}
+              placeholder={"password"}
+              value={password}
+              onChange={(e) => setPassword(e.currentTarget.value)}
+              required
+            ></input>
+            <input
+              type={"text"}
+              placeholder={"Band Name"}
+              value={name}
+              onChange={(e) => setName(e.currentTarget.value)}
+              required
+            ></input>
+            <input
+              type={"text"}
+              placeholder={"genre"}
+              value={genre}
+              onChange={(e) => setGenre(e.currentTarget.value)}
+              required
+            ></input>
+            <input
+              type={"text"}
+              placeholder={"contact"}
+              value={contact}
+              onChange={(e) => setContact(e.currentTarget.value)}
+              required
+            ></input>
+            <input
+              type={"text"}
+              placeholder={"bio"}
+              value={bio}
+              onChange={(e) => setBio(e.currentTarget.value)}
+            ></input>
+            <input
+              type={"text"}
+              placeholder={"city"}
+              value={city}
+              onChange={(e) => setCity(e.currentTarget.value)}
+            ></input>
+          </div>
+          {/* Set Range: */}
+          {/* <select>
                     <option value={"$"}>$</option>
                     <option value={"$$"}>$$</option>
                     <option value={"$$$"}>$$$</option>
                     <option value={"Ask"}>Ask</option>
                 </select> */}
-                <div className="artistUploadImg">
-                <h4>Upload Image</h4>
-                </div>
-                <div className="artistImgUploadBttn">
-                <button onClick={(e) => setProfilePicUrl(e.currentTarget.value)}>Upload</button>
-                </div>
-                <div className="artistSignUpBttn">
-                <button type={"submit"}>Sign Up</button>
-                </div>
-            </form>
-        </div>
-    )
-}
+          <div className="artistUploadImg">
+            <h4>Upload Image</h4>
+          </div>
+          <div className="artistImgUploadBttn">
+            <button onClick={(e) => setProfilePicUrl(e.currentTarget.value)}>
+              Upload
+            </button>
+          </div>
+          <div className="artistSignUpBttn">
+            <button type={"submit"}>Sign Up</button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default ArtistSignUpForm;
