2d0c1f1c59cef43f281cef65e3283c21d6d07814
Ashya <ashyamanning@pursuit.org>
2020-07-12 00:13:15 -0400

    styling login form

--- frontend/src/features/login/Login.js
+++ frontend/src/features/login/Login.js
@@ -31,44 +31,44 @@ const Login = () => {
   return (
     <Modal isOpen={true} onRequestClose={closeModal} isOpen={isOpen} style={{
       content: {
-        backgroundColor: '#F4D8CD',
+        backgroundColor: '#ec9b59',
         borderRadius: '13px',
         left: '25%',
         right: '25%',
         },
       overlay: {
-        backgroundColor: '#164444'
+        backgroundColor: '#00202b'
       }
     }}>
       <div className="loginTitle">
-        <h3 id="loginH3">Log In</h3>
+        <h3 id="loginH3">Roadie Login</h3>
       </div>
       <div className="loginForm">
       {error ? <div>{error}</div> : null}
       <form onSubmit={handleSubmit}>
-      <div className="loginInput">
-        <input
-          className="loginInputSpace"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => {
-            setEmail(e.target.value);
-          }}
-        />
-        <input
-          type="password"
-          className="loginInputSpace"
-          value={password}
-          placeholder="Password"
-          onChange={(e) => setPassword(e.currentTarget.value)}
-        />
-      </div>
-      <div className="loginSubmitBtn">
-        <button type="submit">Login</button>
-      </div>
-      {/* <div className="signupBtn">
-        <button onClick={history.push("/signup")}>SignUp</button>
-      </div> */}
+        <div className="loginInput">
+          <input
+            className="loginInputSpace"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+          />
+          <input
+            type="password"
+            className="loginInputSpace"
+            value={password}
+            placeholder="Password"
+            onChange={(e) => setPassword(e.currentTarget.value)}
+          />
+        </div>
+        <div className="loginSubmitBtn">
+          <button type="submit">Login</button>
+        </div>
+        {/* <div className="signupBtn">
+          <button onClick={history.push("/signup")}>SignUp</button>
+        </div> */}
       </form>
       </div>
     </Modal>
