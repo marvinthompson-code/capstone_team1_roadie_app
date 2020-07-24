9411a165996ed9a8f6a4d4a22e4530c6b230daa8
Henrynunez112 <henrynunez@pursuit.org>
2020-07-23 22:32:38 -0400

    skeleton for artist portfolio done

---
+++ frontend/src/features/Portfolio/Portfolio.js
@@ -0,0 +1,39 @@
+import React from "react";
+
+const ArtistPortfolio = () =>{
+
+    return(
+        <div>
+            <div>
+                <img/>
+                <button>Book Me!</button>
+            </div>
+            <div>
+            <h1>Artist Name</h1>
+            </div>
+            <div>
+                <h4>Bio</h4>
+            </div>
+            <div>
+                <h4>Upcoming Shows</h4>
+                <ul>
+                    <li>mm/dd/yyyy</li>
+                    <li>mm/dd/yyyy</li>
+                    <li>mm/dd/yyyy</li>
+                    <li>mm/dd/yyyy</li>
+                    <li>mm/dd/yyyy</li>
+                </ul>
+            </div>
+            <div>
+                <h2>Artist Album</h2>
+                <button>Upload Picture</button>
+            </div>
+            <div>
+                <h2>Artist Videos</h2>
+                <button>Upload Picture</button>
+            </div>
+        </div>
+    )
+
+}
+export default ArtistPortfolio; 
