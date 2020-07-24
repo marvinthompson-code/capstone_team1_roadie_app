68ed583f26b77b68284fe68cd84a4b7a4f7584a3
Henrynunez112 <henrynunez@pursuit.org>
2020-07-23 22:53:08 -0400

    more skelleton work

--- frontend/src/features/Portfolio/Portfolio.js
+++ frontend/src/features/Portfolio/Portfolio.js
@@ -3,18 +3,18 @@ import React from "react";
 const ArtistPortfolio = () =>{
 
     return(
-        <div>
-            <div>
+        <div className="artistPortfolioContainer">
+            <div className="portfolioHeader">
                 <img/>
                 <button>Book Me!</button>
             </div>
-            <div>
+            <div className="artistName">
             <h1>Artist Name</h1>
             </div>
-            <div>
+            <div className="artistBio">
                 <h4>Bio</h4>
             </div>
-            <div>
+            <div className="artistShowDates">
                 <h4>Upcoming Shows</h4>
                 <ul>
                     <li>mm/dd/yyyy</li>
@@ -24,13 +24,15 @@ const ArtistPortfolio = () =>{
                     <li>mm/dd/yyyy</li>
                 </ul>
             </div>
-            <div>
+            <div className="artistMediaContainer">
+            <div className="artistAlbumDiv">
                 <h2>Artist Album</h2>
                 <button>Upload Picture</button>
             </div>
-            <div>
+            <div className="artistVideoDiv">
                 <h2>Artist Videos</h2>
                 <button>Upload Picture</button>
+            </div>
             </div>
         </div>
     )
