2c8a627a5f3d85576cf8c4d5b5119420fb89b04d
Henrynunez112 <henrynunez@pursuit.org>
2020-07-25 18:33:10 -0400

    uploading picture into artist album

--- frontend/src/features/Portfolio/Portfolio.js
+++ frontend/src/features/Portfolio/Portfolio.js
@@ -1,63 +1,113 @@
-import React, {useState, useEffect} from "react";
-import axios from 'axios';
-import {apiURL} from '../../util/apiURL';
-import {useRouteMatch} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { storage } from "../../firebase";
+import { apiURL } from "../../util/apiURL";
+import { useRouteMatch } from "react-router-dom";
 
+const ArtistPortfolio = () => {
+  const [name, setName] = useState("");
+  const [videos, setVideos] = useState([]);
+  const [pictures, setPictures] = useState([]);
+  const [profilePic, setProfilePic] = useState("");
 
-const ArtistPortfolio = () =>{
-    const [name, setName] = useState("");
-    const [videos, setVideos] = useState([]);
-    const [pictures, setPictures] = useState([]);
-    const [profilePic, setProfilePic] = useState("");
+  const API = apiURL();
+  const match = useRouteMatch();
 
+  //imageUpload
+  const [imageAsFile, setImageAsFile] = useState("");
+  const [imageUrl, setImageUrl] = useState("");
+  const [toggleUploadMsg, setToggleUploadMsg] = useState(false);
 
-    const API = apiURL();
-    const match = useRouteMatch()
+  const handleImageAsFile = (e) => {
+    const image = e.target.files[0];
+    const types = ["image/png", "image/jpeg", "image/jpg"];
+    if (types.every((type) => image.type !== type)) {
+      alert(`${image.type} is not a supported format`);
+    } else {
+      setImageAsFile((imageFile) => image);
+    }
+  };
 
-    useEffect(() =>{
-        const fetchArtist = async (id) =>{
-            let res = await axios.get(`${API}/artists/${id}`);
-            let {name, profile_pic_url} = res.data.body.single_artist;
-            setName(name);
-            setProfilePic(profile_pic_url);
-        };
-        const fetchArtistPictures = async (id) =>{
-            let res = await axios.get(`${API}/media/artists/${id}/pictures`);
-            let {picture} = res.data.body.picture;
-            setPictures(picture);
-        };
-        const fetchArtistVideos = async (id) =>{
-            let res = await axios.get(`${API}/media/artists/${id}/videos`);
-            let {video} = res.data.body.video;
-            setVideos(video);
-        };
-        fetchArtist(match.params.id);
-        fetchArtistPictures(match.params.id);
-        fetchArtistVideos(match.params.id)
+  const handleFirevaseUpload = () => {
+    if (imageAsFile === "") {
+      alert(`Please choose a valid file before uploading`);
+    } else if (imageAsFile !== null) {
+      const uploadTask = storage
+        .ref(`/images/${imageAsFile.name}`)
+        .put(imageAsFile);
+      uploadTask.on(
+        "state_changed",
+        (snapShot) => {
+          var progress =
+            (snapShot.bytesTransferred / snapShot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
+          console.log(snapShot);
+        },
+        (err) => {
+          console.log(err);
+        },
+        () => {
+          storage
+            .ref("images")
+            .child(imageAsFile.name)
+            .getDownloadURL()
+            .then((fireBaseUrl) => {
+              setImageUrl(fireBaseUrl);
+            });
+        }
+      );
+      setToggleUploadMsg(true);
+    }else{
+        setToggleUploadMsg(false);
+    }
+  };
 
-    },[])
+  
 
-    return(
-        <div className="artistPortfolioContainer">
-            <div className="portfolioHeader">
-                <img src={profilePic}/>
-                <button>Book Me!</button>
-            </div>
-            <div className="artistName">
-            <h1>{name}</h1>
-            </div>
-            <div className="artistMediaContainer">
-            <div className="artistAlbumDiv">
-                <h2>{name}'s Album</h2>
-                <button>Upload Picture</button>
-            </div>
-            <div className="artistVideoDiv">
-                <h2>{name}'s Videos</h2>
-                <button>Upload Picture</button>
-            </div>
-            </div>
+  useEffect(() => {
+    const fetchArtist = async (id) => {
+      let res = await axios.get(`${API}/artists/${id}`);
+      let { name, profile_pic_url } = res.data.body.single_artist;
+      setName(name);
+      setProfilePic(profile_pic_url);
+    };
+    const fetchArtistPictures = async (id) => {
+      let res = await axios.get(`${API}/media/artists/${id}/pictures`);
+      let { picture } = res.data.body.picture;
+      setPictures(picture);
+    };
+    const fetchArtistVideos = async (id) => {
+      let res = await axios.get(`${API}/media/artists/${id}/videos`);
+      let { video } = res.data.body.video;
+      setVideos(video);
+    };
+    fetchArtist(match.params.id);
+    fetchArtistPictures(match.params.id);
+    fetchArtistVideos(match.params.id);
+  }, []);
+
+  return (
+    <div className="artistPortfolioContainer">
+      <div className="portfolioHeader">
+        <img src={profilePic} />
+        <button>Book Me!</button>
+      </div>
+      <div className="artistName">
+        <h1>{name}</h1>
+      </div>
+      <div className="artistMediaContainer">
+        <div className="artistAlbumDiv">
+          <h2>{name}'s Album</h2>
+          <input type="file" required onChange={handleImageAsFile} />
+          <button type="button" onClick={handleFirevaseUpload}>Upload Picture</button>
+          {toggleUploadMsg ? <h5>Upload successful!</h5> : null}
         </div>
-    )
-
-}
-export default ArtistPortfolio; 
+        <div className="artistVideoDiv">
+          <h2>{name}'s Videos</h2>
+          <button>Upload Video</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default ArtistPortfolio;
