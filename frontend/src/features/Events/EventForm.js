a10de5850ec39c443deef1cb29294e8cd9e6d4d9
Kevin Wong <kevinwong@pursuit.org>
2020-07-28 20:09:05 -0400

    work on event form css

--- frontend/src/features/Events/EventForm.js
+++ frontend/src/features/Events/EventForm.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from "react";
 import { apiURL } from "../../util/apiURL";
-import { toggleEventModalState } from './eventModalSlice'
-import { useSelector, useDispatch } from 'react-redux'
-import { useHistory } from 'react-router-dom'
+import { toggleEventModalState } from "./eventModalSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import axios from "axios";
-import Modal from 'react-modal'
+import Modal from "react-modal";
+import "../../css/EventForm.css";
 
 const EventForm = () => {
   const [name, setName] = useState("");
@@ -12,14 +13,14 @@ const EventForm = () => {
   const [date, setDate] = useState("");
   const [address, setAddress] = useState("");
   const [city, setCity] = useState("");
-  const dispatch = useDispatch()
-  const client = useSelector(state => state.client)
-  const history = useHistory()
-  const isOpen = useSelector(state => state.eventModal)
+  const dispatch = useDispatch();
+  const client = useSelector((state) => state.client);
+  const history = useHistory();
+  const isOpen = useSelector((state) => state.eventModal);
   const API = apiURL();
 
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
     try {
       let res = await axios.post(`${API}/events/`, {
         name,
@@ -29,78 +30,75 @@ const EventForm = () => {
         city,
         client_id: client.id,
       });
-      debugger
     } catch (err) {
       console.log(err);
     }
   };
 
   const closeModal = () => {
-    dispatch(toggleEventModalState())
-}
-
+    dispatch(toggleEventModalState());
+  };
 
   return (
     <Modal
-    isOpen={false}
-         onRequestClose={closeModal}
-         isOpen={isOpen}
-         style={{
-            content: {
-                backgroundColor: "#F4D8CD",
-                borderRadius: "13px",
-                left: "25%",
-                right: "25%",
-              }
-         }}
+      isOpen={false}
+      onRequestClose={closeModal}
+      isOpen={isOpen}
+      style={{
+        content: {
+          backgroundColor: "#F4D8CD",
+          borderRadius: "13px",
+          left: "25%",
+          right: "25%",
+        },
+      }}
     >
-
-    <div className="eventFormDiv">
-      <form className="eventForm" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          className="eventFormInput"
-          value={name}
-          placeholder="Name of Event"
-          onChange={(e) => setName(e.currentTarget.value)}
-          required
+      <div className="eventFormDiv">
+        <form className="eventForm" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            className="eventFormInput"
+            value={name}
+            placeholder="Name of Event"
+            onChange={(e) => setName(e.currentTarget.value)}
+            required
           />
-        <input
-          type="text"
-          className="eventFormInput"
-          value={venue}
-          placeholder="Venue"
-          onChange={(e) => setVenue(e.currentTarget.value)}
-          required
+          <input
+            type="text"
+            className="eventFormInput"
+            value={venue}
+            placeholder="Venue"
+            onChange={(e) => setVenue(e.currentTarget.value)}
+            required
           />
-        <input
-          type="text"
-          className="eventFormInput"
-          value={date}
-          placeholder="Date"
-          onChange={(e) => setDate(e.currentTarget.value)}
-          required
+          <input
+            type="text"
+            className="eventFormInput"
+            value={date}
+            placeholder="Date"
+            onChange={(e) => setDate(e.currentTarget.value)}
+            required
           />
-        <input
-          type="text"
-          className="eventFormInput"
-          value={address}
-          placeholder="Address"
-          onChange={(e) => setAddress(e.currentTarget.value)}
-          required
+          <input
+            type="text"
+            className="eventFormInput"
+            value={address}
+            placeholder="Address"
+            onChange={(e) => setAddress(e.currentTarget.value)}
+            required
           />
-        <input
-          type="text"
-          className="eventFormInput"
-          value={city}
-          placeholder="City"
-          onChange={(e) => setCity(e.currentTarget.value)}
-          required
+          <input
+            type="text"
+            className="eventFormInput"
+            value={city}
+            placeholder="City"
+            onChange={(e) => setCity(e.currentTarget.value)}
+            required
           />
-        <button type="submit">Add Event</button>
-      </form>
-    </div>
-          </Modal>
+          <button type="submit">Add Event</button>
+        </form>
+      </div>
+    </Modal>
   );
 };
 
--- frontend/src/features/navbar/NavBar.js
+++ frontend/src/features/navbar/NavBar.js
@@ -1,29 +1,44 @@
 import React, { useContext } from "react";
 import { NavLink } from "react-router-dom";
-import { toggleModalState } from '../Artist/modalSlice'
-import { clientLogout } from '../token/clientTokenSlice'
-import { artistLogout } from '../token/artistTokenSlice'
-import { useDispatch } from 'react-redux'
+import { toggleModalState } from "../Artist/modalSlice";
+import { clientLogout } from "../token/clientTokenSlice";
+import { artistLogout } from "../token/artistTokenSlice";
+import { useDispatch } from "react-redux";
 import "../../css/NavBar.css";
 import { logout } from "../../util/firebaseFunctions";
 import { AuthContext } from "../../providers/AuthContext";
 
 const NavBar = () => {
   const { currentUser } = useContext(AuthContext);
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
+
   const userLogout = () => {
-    dispatch(clientLogout())
-    dispatch(artistLogout())
-    logout()
-  }
+    dispatch(clientLogout());
+    dispatch(artistLogout());
+    logout();
+  };
+
   const displayButtons = () => {
+    debugger;
     if (currentUser) {
-      return <button onClick={userLogout}>Logout</button>;
+      return (
+        <>
+          <button onClick={userLogout}>Logout</button>;
+        </>
+      );
     } else {
       return (
         <>
-          <NavLink exact to={"/login"} onClick={() => dispatch(toggleModalState())}>Login</NavLink>
-          <NavLink to={"/signup"} onClick={() => dispatch(toggleModalState())}>Sign Up</NavLink>
+          <NavLink
+            exact
+            to={"/login"}
+            onClick={() => dispatch(toggleModalState())}
+          >
+            Login
+          </NavLink>
+          <NavLink to={"/signup"} onClick={() => dispatch(toggleModalState())}>
+            Sign Up
+          </NavLink>
         </>
       );
     }
