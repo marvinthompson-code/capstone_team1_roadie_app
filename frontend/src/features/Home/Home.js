a260e08a46524c933def208994b5110fa66a0441
Henrynunez112 <henrynunez@pursuit.org>
2020-07-29 13:22:08 -0400

    homepage css touch up

--- frontend/src/features/Home/Home.js
+++ frontend/src/features/Home/Home.js
@@ -1,29 +1,25 @@
-import React from 'react'
-import Search from './Search'
+import React from "react";
+import Search from "./Search";
 import BookMeForm from "../Artist/BookMeForm";
 import "../../css/Home.css";
 
-
 const Home = () => {
-
-    return(
-        <div className="homeContainer">
-
-            <div className="searchHomeContainer">
-                <div className="pinkContainer">
-                <h1 id="titleHeader">
-                WELCOME TO ROADIE
-                </h1>
-                <Search />
-                </div>
-            </div>
-
-            <div className="bookMeFormContainer">
-                <BookMeForm />
-            </div>
+  return (
+    <div className="homeContainer">
+      <div className="searchHomeContainer">
+        <div className="pinkContainer">
+          <div className="titleContainer">
+            <h1 id="titleHeader">WELCOME TO ROADIE</h1>
+          </div>
+          <Search />
         </div>
-    )
-}
+      </div>
 
-export default Home
+      <div className="bookMeFormContainer">
+        <BookMeForm />
+      </div>
+    </div>
+  );
+};
 
+export default Home;
