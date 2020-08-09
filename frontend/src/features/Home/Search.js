f88df3bc7531532b6af08ee29b3715bec9ac9a37
Marvin <marvinthompson@pursuit.org>
2020-08-09 14:14:09 -0400

    changes

--- frontend/src/features/Home/Search.js
+++ frontend/src/features/Home/Search.js
@@ -21,11 +21,14 @@ const Search = () => {
             let routeExtension = name === "" ? "" : `/search/${name}`
             if (userType === "Artist") {
                 let res = await axios.get(`${API}/artists` + routeExtension);
+                debugger
                 dispatch(receiveSearch(res.data.body.artists));
             } else if (userType === "Client") {
                 let res = await axios.get(`${API}/clients` + routeExtension);
+                debugger
                 dispatch(receiveSearch(res.data.body.clients));
             }
+            debugger
             dispatch(receiveUserType(userType));
             history.push("/results");
             
@@ -38,7 +41,7 @@ const Search = () => {
         <div className="searchContainer">
             <form onSubmit={handleSubmit} className="searchForm">
                 <input type={"text"} value={name} placeholder={"Name"} onChange={(e) => setName(e.target.value)} id="searchInput"/>
-                <select value={userType} onChange={(e)=> setUserType(e.target.value)} id="searchSelect">
+                <select value={userType} required aria-required={"true"} onChange={(e)=> setUserType(e.target.value)} id="searchSelect">
                     <option disabled value="">Search By...</option>
                     <option value={"Artist"} >Artist</option>
                     <option value={"Client"}>Client</option>
