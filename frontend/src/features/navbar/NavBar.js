2fc5dbc0166b399630514879d397163d2aaa3f3b
Marvin <marvinthompson@pursuit.org>
2020-08-28 20:40:42 -0400

    navbar

--- frontend/src/features/navbar/NavBar.js
+++ frontend/src/features/navbar/NavBar.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { NavLink, useHistory } from "react-router-dom";
 import { toggleLoginModalState } from "../login/loginModalSlice";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { clientLogout } from "../token/clientTokenSlice";
 import { artistLogout } from "../token/artistTokenSlice";
 import { recieveToken } from "../token/tokenSlice";
@@ -8,6 +9,7 @@ import { useDispatch, useSelector } from "react-redux";
 import "../../css/NavBar.css";
 import { logout } from "../../util/firebaseFunctions";
 import { AuthContext } from "../../providers/AuthContext";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
 
 const NavBar = () => {
   const { currentUser } = useContext(AuthContext);
@@ -21,7 +23,7 @@ const NavBar = () => {
     if (client === null && artist !== null) {
       return (
         <li className="nav-item active">
-          <NavLink exact to={`/artist/${currentUser.id}`} className="nav-link">
+          <NavLink exact to={`/artist/${currentUser.id}`} className="nav-link profileTab">
             Profile
           </NavLink>
         </li>
@@ -29,7 +31,7 @@ const NavBar = () => {
     } else if (client !== null && artist === null) {
       return (
         <li className="nav-item active">
-          <NavLink exact to={`/client/${currentUser.id}`} className="nav-link">
+          <NavLink exact to={`/client/${currentUser.id}`} className="nav-link profileTab">
             Profile
           </NavLink>
         </li>
@@ -50,15 +52,18 @@ const NavBar = () => {
     if (currentUser) {
       return (
         <>
-          <li className="nav-item active">{routeExt()}</li>
-
-          <button
-            className="logoutBttn justify-content-end"
-            id="roadieLogout"
-            onClick={userLogout}
-          >
-            Logout
-          </button>
+          {routeExt()}
+          <li className="nav-item ">
+            <NavLink
+              className="nav-link logoutBttn"
+              href="#"
+              id="roadieLogout"
+              onClick={userLogout}
+              exact to="/"
+            >
+              Logout <span className="sr-only">(current)</span>
+            </NavLink>
+          </li>
         </>
       );
     } else {
@@ -96,7 +101,7 @@ const NavBar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light roadieNav sticky-top">
       <NavLink className="navbar-brand navTitle" href="#" exact to="/">
-      <span id="roadieR">R</span>oadie
+        <span id="roadieR">R</span>oadie
       </NavLink>
       <button
         className="navbar-toggler"
