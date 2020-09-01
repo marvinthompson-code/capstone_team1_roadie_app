cacdc481c95b4ddb9dc9cb69fedbb312c590b8a2
Henrynunez112 <henrynunez@pursuit.org>
2020-09-01 17:22:11 -0400

    created a switch for the signUp page

--- frontend/src/features/Display/DisplaySignUp.js
+++ frontend/src/features/Display/DisplaySignUp.js
@@ -1,27 +1,10 @@
 import React, { useState } from "react";
-import ArtistSignUpForm from "../Artist/ArtistSignUpForm";
-import ClientSignUp from "../client/clientSignUp";
-import "../../css/DisplaySignUp.css"
+// import ArtistSignUpForm from "../Artist/ArtistSignUpForm";
+// import ClientSignUp from "../client/clientSignUp";
+import "../../css/DisplaySignUp.css";
+import SignUpToggle from "../SignUpToggle/SignUpToggle";
 
 const DisplaySignUp = () => {
-  const [toggle, setToggle] = useState(false);
-  const onClick = () => {
-    if (!toggle) {
-      setToggle(true);
-    } else {
-      setToggle(false);
-    }
-  };
-  const displayModal = () => {
-    return (
-      <div className="displaySignUpContainer">
-        <div className="buttonContainer">
-        </div>
-        {toggle ? <ArtistSignUpForm /> : <ClientSignUp />}
-      </div>
-    );
-  };
-
   return (
     <div className="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
       <div className="modal-dialog modal-dialog-centered" role="document">
@@ -35,18 +18,7 @@ const DisplaySignUp = () => {
           </div>
           
           <div className="modal-body signUpModalBody">
-    {/* <ul className="nav nav-tabs">
-    <li className="nav-item">
-    <a className="nav-link active" id="signUpTabs"href="#" onClick={onClick}>Client</a>
-  </li>
-  <li className="nav-item">
-    <a className="nav-link active" id="signUpTabsArtist" href="#" onClick={onClick}>Artist</a>
-  </li>
-  
-  </ul> */}
-  <input type="checkbox" checked data-toggle="toggle" data-on="Hello<br>World" data-off="Goodbye<br>World"/>
-
-      {displayModal()}
+    <SignUpToggle />
     </div>
     <div className="modal-footer signUpModalFooter">
          <button type="button" className="signUpCloseButton btn-secondary" data-dismiss="modal">Close</button>
