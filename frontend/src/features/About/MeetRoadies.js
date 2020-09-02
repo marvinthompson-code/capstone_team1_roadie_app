da355fc4d80a7110e4af7ee3034ceeb3b99f8bb4
Henrynunez112 <henrynunez@pursuit.org>
2020-09-01 20:51:09 -0400

    carousel for meet the roadies created

--- frontend/src/features/About/MeetRoadies.js
+++ frontend/src/features/About/MeetRoadies.js
@@ -1,17 +1,69 @@
 import React from "react";
+import "../../css/MeetRoadies.css";
 
 const MeetTheRoadies = () => {
-    return (
-        <div class="jumbotron jumbotron-fluid">
-            <div class="container">
-                <h1 class="display-4">Meet the Roadies!</h1>
-                <p class="lead">We are the founding creators of Roadie! We came together on a solid commonality: our love for music! In that, we brainstormed some ideas... A team member, Marvin, led the way in adopting this prospect of building an alliance with musicians and agents to be able to lessen the gap between bookings gigs, event management, and actually being seen by the clients creating those events. </p>
-            </div>
-            <div>
-                
-            </div>
+  return (
+    <div className="container meetTheRoadies">
+      <div className="jumbotron jumbotron-fluid meetTheRoadiesJumbo">
+        <div class="container meetTheRoadiesHeader">
+          <h1 className="display-4">
+            Meet the <span id="WhyR">R</span>oadies!
+          </h1>
+          <p className="lead meetTheRoadiesPTag">
+            We are the founding creators of Roadie! We came together on a solid
+            commonality: our love for music! In that, we brainstormed some
+            ideas... A team member, Marvin, led the way in adopting this
+            prospect of building an alliance with musicians and agents to be
+            able to lessen the gap between bookings gigs, event management, and
+            actually being seen by the clients creating those events.{" "}
+          </p>
         </div>
-    )
+        <div className="meetTheRoadiesDiv">
+          <div
+            id="carouselExampleControls"
+            className="meetTheRoadiesCar carousel slide"
+            data-ride="carousel"
+          >
+            <div className="carousel-inner">
+              <div className="carousel-item active">
+                <img className="d-block w-100" src="..." alt="First slide" />
+              </div>
+              <div className="carousel-item">
+                <img className="d-block w-100" src="..." alt="Second slide" />
+              </div>
+              <div className="carousel-item">
+                <img className="d-block w-100" src="..." alt="Third slide" />
+              </div>
+            </div>
+            <a
+              className="carousel-control-prev"
+              href="#carouselExampleControls"
+              role="button"
+              data-slide="prev"
+            >
+              <span
+                className="carousel-control-prev-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="sr-only">Previous</span>
+            </a>
+            <a
+              className="carousel-control-next"
+              href="#carouselExampleControls"
+              role="button"
+              data-slide="next"
+            >
+              <span
+                className="carousel-control-next-icon"
+                aria-hidden="true"
+              ></span>
+              <span className="sr-only">Next</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default MeetTheRoadies;
