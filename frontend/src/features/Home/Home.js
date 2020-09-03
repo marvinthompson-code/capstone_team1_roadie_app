4c5967e13314c273e72fca865eb3f84845419453
Ashya <ashyamanning@pursuit.org>
2020-09-03 14:23:14 -0400

    update home jumbotron with carousel

--- frontend/src/features/Home/Home.js
+++ frontend/src/features/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Search from "./Search";
 import { useSelector } from "react-redux";
+import { NavLink } from "react-router-dom";
 import "../../css/Home.css";
 import guitar from '../images/rhcp.jpg'
 import performer from '../images/performer.jpg'
@@ -29,67 +30,112 @@ const Home = () => {
         <p className="lead display-5 pTagHeader">
           With Roadie, your next packed gig is only moments away.{" "}
         </p>
-        <div
-          id="carouselExampleIndicators"
-          className="carousel slide Homecarousel"
-          data-ride="carousel"
-        >
-          <ol className="carousel-indicators">
-            <li
-              data-target="#carouselExampleIndicators"
-              data-slide-to="0"
-              className="active"
-            ></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
+        <div className="missionContainer">
+          <div className="row">
+            <div className="col leftPec">
+              <div
+                id="carouselExampleIndicators"
+                className="carousel slide homeCarousel"
+                data-ride="carousel"
+              >
+                <ol className="carousel-indicators">
+                  <li
+                    data-target="#carouselExampleIndicators"
+                    data-slide-to="0"
+                    className="active"
+                  ></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                  <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                </ol>
 
-          <div className="carousel-inner">
-            <div className="carousel-item active">
-              <img src={guitar} alt="Guitar Player" className={"slideshowImage"}/>
-              <div className="carousel-caption homeRoadingCaption d-none d-md-block">
-                <h5>Artists</h5>
-                <p>Connect with Booking Agents. Get gigs. Play shows. </p>
-              </div>
-            </div>
+                <div className="carousel-inner">
+                  <div className="carousel-item active">
+                    <img src={guitar} alt="Guitar Player" className={"slideshowImage"}/>
+                    <div className="carousel-caption homeRoadingCaption d-none d-md-block">
+                      <h5>Artists</h5>
+                      <p>Connect with Booking Agents. Get gigs. Play shows. </p>
+                    </div>
+                  </div>
 
-            <div className="carousel-item">
-              <img src={headshot} alt="Woman with Afro" className={"slideshowImage"} />
-              <div className="carousel-caption homeRoadingCaption d-none d-md-block">
-                <h5>Clients</h5>
-                <p>
-                  Seemless Event Creation. Find that next star for your stage.
-                </p>
-              </div>
-            </div>
+                  <div className="carousel-item">
+                    <img src={headshot} alt="Woman with Afro" className={"slideshowImage"} />
+                    <div className="carousel-caption homeRoadingCaption d-none d-md-block">
+                      <h5>Clients</h5>
+                      <p>
+                        Seamless Event Creation. Find that next star for your stage.
+                      </p>
+                    </div>
+                  </div>
 
-            <div className="carousel-item">
-              <img src={concert} alt="Blue Concert Outdoor" className={"slideshowImage"}/>
-              <div className="carousel-caption homeRoadingCaption d-none d-md-block">
-                <h5>Grow</h5>
-                <p>Customize profies. Promote events. Grow together.</p>
+                  <div className="carousel-item">
+                    <img src={concert} alt="Blue Concert Outdoor" className={"slideshowImage"}/>
+                    <div className="carousel-caption homeRoadingCaption d-none d-md-block">
+                      <h5>Grow</h5>
+                      <p>Customize profiles. Promote events. Grow together.</p>
+                    </div>
+                  </div>
+                </div>
+
+                <a
+                  className="carousel-control-prev"
+                  href="#carouselExampleIndicators"
+                  role="button"
+                  data-slide="prev"
+                >
+                  <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                  <span className="sr-only homeSlideBtn">Previous</span>
+                </a>
+                <a
+                  className="carousel-control-next"
+                  href="#carouselExampleIndicators"
+                  role="button"
+                  data-slide="next"
+                >
+                  <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                  <span className="sr-only homeSlideBtn">Next</span>
+                </a>
               </div>
             </div>
           </div>
-
-          <a
-            className="carousel-control-prev"
-            href="#carouselExampleIndicators"
-            role="button"
-            data-slide="prev"
-          >
-            <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span className="sr-only homeSlideBtn">Previous</span>
-          </a>
-          <a
-            className="carousel-control-next"
-            href="#carouselExampleIndicators"
-            role="button"
-            data-slide="next"
-          >
-            <span className="carousel-control-next-icon" aria-hidden="true"></span>
-            <span className="sr-only homeSlideBtn">Next</span>
-          </a>
+          <div className="row">
+            <div className="col rightPec">
+              <div className="roadieLure">
+                <h5 className="lead display-4">Are you a Roadie?</h5>
+                <p className="lead display-5">
+                  <span className="badge badge-success">R</span>
+                  Want to be in full control of your talent?
+                </p>
+                <p className="lead display-5">
+                  <span className="badge badge-success">R</span>
+                  Ever light up with excitement about coming opportunities?
+                </p>
+                <p className="lead display-5">
+                  <span className="badge badge-success">R</span>
+                  Do you have the grit to take your career to the next level?
+                </p>
+                <p className="lead display-5">
+                  <span className="badge badge-success">R</span>
+                  Are you ready to set the stage of your wildest dreams?
+                </p>
+                <p className="lead display-5">
+                <span className="badge badge-success">R</span>
+                  What will you do for it?
+                </p>
+                <div className="homeSignUpBtnDiv">
+                  <NavLink
+                    className="nav-link roadieHomeSignUp"
+                    href="#"
+                    exact
+                    to="/"
+                    data-toggle="modal"
+                    data-target="#exampleModalCenter"
+                  >
+                    Become a Roadie NOW! <span className="sr-only">(current)</span>
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
