09c8fb2a9cf764a46392529a08b5a3495d4fc549
Henrynunez112 <henrynunez@pursuit.org>
2020-09-05 12:16:51 -0400

    component for ClientVideoPlayer created

---
+++ frontend/src/features/Albums/VideoPlayer/ClientVideoPlayer.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from "react";
+import { useRouteMatch } from "react-router-dom";
+import { apiURL } from "../../../util/apiURL";
+import ReactPlayer from "react-player";
+import axios from "axios";
+
+const ClientVideoPlayer = () => {
+  const [user, setUser] = useState({});
+  const match = useRouteMatch();
+  const API = apiURL();
+
+  useEffect(() => {
+    debugger;
+    const fetchClientVideo = async (client_id, id) => {
+      let res = await axios.get(
+        `${API}/media/videos/client/${client_id}/video/${id}`
+      );
+      debugger;
+      setUser(res.data.body.singleVideo);
+    };
+    fetchClientVideo(match.params.client_id, match.params.id);
+  }, []);
+
+  return (
+    <div>
+      <h1>{user.caption}</h1>
+      <ReactPlayer className="react-player" url={user.url} controls />
+    </div>
+  );
+};
+export default ClientVideoPlayer;
