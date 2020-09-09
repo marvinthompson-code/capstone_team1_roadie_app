5b2167f13ad4e58468e0fd3089616afdde2eb97c
Kevin Wong <kevinwong@pursuit.org>
2020-09-08 20:42:11 -0400

    work on toasts

--- frontend/src/features/Artist/BookMeForm.js
+++ frontend/src/features/Artist/BookMeForm.js
@@ -21,7 +21,6 @@ const BookMeForm = () => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // make notification request here
     await db
       .collection("bookings")
       .doc(match.params.id)
