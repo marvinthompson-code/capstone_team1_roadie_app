28c05f9bac150958215e5699227239befb31724c
Kevin Wong <kevinwong@pursuit.org>
2020-09-11 01:26:50 -0400

    align bullets on whyRoadie to left, consistency with home page

--- frontend/src/features/About/WhyRoadie.js
+++ frontend/src/features/About/WhyRoadie.js
@@ -6,7 +6,9 @@ const WhyRoadie = () => {
     <div className="container whyRoadie">
       <div class="jumbotron jumbotron-fluid text-center whyRoadieJumbotron">
         <div class="row justify-content-center text-center whyRoadieHeader">
-          <h1 class="display-4" id="whyRh">So, why <span id="WhyR">R</span>oadie?</h1>
+          <h1 class="display-4" id="whyRh">
+            So, why <span id="WhyR">R</span>oadie?
+          </h1>
         </div>
         <div className="row justify-content-center text-center whyRoadieSubHeader">
           <h4 id="whyRH4">
@@ -17,36 +19,35 @@ const WhyRoadie = () => {
           </h4>
         </div>
         <div className="row justify-content-center text-center whyRUl">
-        <ul id="whyRUl">
-          <div>
-            <li>
-              {" "}
-              <p>
-              <span className="badge badge-success whyBadge">R</span>
-                There are too many outside parties involved in negotiating
-                affairs.
-              </p>
-            </li>
+          <ul id="whyRUl">
+            <div style={{ textAlign: "left" }}>
+              <li>
+                {" "}
+                <p>
+                  <span className="badge badge-success whyBadge">R</span> There
+                  are too many outside parties involved in negotiating affairs.
+                </p>
+              </li>
 
-            <li>
-              {" "}
-              <p>
-              <span className="badge badge-success whyBadge">R</span>
-                There is no existing road map for musicians to book and plan
-                effectively.
-              </p>
-            </li>
+              <li>
+                {" "}
+                <p>
+                  <span className="badge badge-success whyBadge">R</span> There
+                  is no existing road map for musicians to book and plan
+                  effectively.
+                </p>
+              </li>
 
-            <li>
-              {" "}
-              <p>
-              <span className="badge badge-success whyBadge">R</span>
-                There is no standing application to handle booking and event
-                management.
-              </p>
-            </li>
-          </div>
-        </ul>
+              <li>
+                {" "}
+                <p>
+                  <span className="badge badge-success whyBadge">R</span> There
+                  is no standing application to handle booking and event
+                  management.
+                </p>
+              </li>
+            </div>
+          </ul>
         </div>
       </div>
     </div>
